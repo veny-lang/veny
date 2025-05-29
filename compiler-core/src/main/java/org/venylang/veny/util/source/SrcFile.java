a56c18684a6a59ac8b10b3319f746d40c84e564f
venylang <stojan.petkov@gmail.com>
2025-05-29 12:31:23 +0300

    Update and rename VenyFile.java to SrcFile.java

---
+++ compiler-core/src/main/java/org/venylang/veny/util/source/SrcFile.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Stoyan Petkov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+public class SrcFile {
+  private final String name;
+  private final int base;
+  private final String content;
+
+  public SrcFile(String name, int base, String content) {
+    this.name = name;
+    this.base = base;
+    this.content = content;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public int base() {
+    return base;
+  }
+
+  public String content() {
+    return content;
+}
